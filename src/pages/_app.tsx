ea636aa6108d1087e7d142414704ea3c76e2c8b5
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-30 15:36:29 -0300

    feat: rename global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
-import GlobalStyle from 'styles/global.styles'
+import GlobalStyle from 'styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
