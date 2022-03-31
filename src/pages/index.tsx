acf62405ddb47a75b5c582386c8200199ab70061
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-31 16:48:30 -0300

    feat: add Home template in Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import Banner from 'components/Banner'
+import Header from 'components/Header'
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return <h1>Home</h1>
+import Home from 'templates/Home'
+
+const props = {
+  title: 'Facilitar a locação de horários de quadras esportivas',
+  description:
+    'Mudou de cidade? Encontre polos esportivos, times e peladas perto de você e <strong>saia para o abraço</strong>.',
+  image: '/img/player-1.svg'
 }
 
-export default Home
+const Index: NextPage = () => {
+  return (
+    <Home>
+      <Header />
+      <Banner {...props} />
+    </Home>
+  )
+}
+
+export default Index
