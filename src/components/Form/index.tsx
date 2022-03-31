f630572c201258baa46b7991b5bc1701d5919c12
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-31 16:54:13 -0300

    feat: add form base component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,70 @@
+import styled, { css } from 'styled-components'
+import { darken } from 'polished'
+
+import * as TextFieldStyles from 'components/TextField/styles'
+import * as ButtonStyles from 'components/Button/styles'
+
+export const FormWrapper = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.Wrapper} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
+
+    ${ButtonStyles.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const FormLoading = styled.img.attrs(() => ({
+  src: '/img/dots.svg',
+  alt: 'Waiting...'
+}))`
+  width: 4rem;
+`
+
+export const FormError = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+    color: red;
+    font-size: ${theme.font.sizes.small};
+
+    svg {
+      width: 1.6rem;
+    }
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.bgColor};
+      color: ${theme.colors.bgColor};
+      transition: color, border, ${theme.transition.fast};
+
+      &:hover {
+        color: ${darken(0.1, theme.colors.bgColor)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.bgColor)};
+      }
+    }
+  `}
+`
+
+export const FormSuccess = styled.p`
+  ${({ theme }) => css`
+    text-align: center;
+    color: ${theme.colors.black};
+    font-size: ${theme.font.sizes.small};
+
+    svg {
+      color: ${theme.colors.secondary};
+      width: 2.4rem;
+    }
+  `}
+`
