64dd6df107d1ace1a99cee46d1d8fa569564fbca
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-31 22:06:16 -0300

    refactor: remove positions

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,50 +4,13 @@ import { InputProps } from './types'
 type IconProps = Pick<InputProps, 'icon'>
 
 export const InputWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  position: absolute;
-`
-
-export const Icon = styled.div`
   ${({ theme }) => css`
     display: flex;
-    color: ${theme.colors.gray};
-    position: relative;
-    left: 32px;
-    z-index: ${theme.layers.modal};
-
-    & > img {
-      width: 2.2rem;
-      height: 100%;
-    }
-  `}
-`
-
-export const Eye = styled.div`
-  position: relative;
-  right: 38px;
-  top: 0.3rem;
-`
-
-export const Error = styled.div``
-
-export const Input = styled.input<IconProps>`
-  ${({ theme }) => css`
+    align-items: center;
     background-color: ${theme.colors.boxBg};
     border: 2px solid #1ac79d;
     border-radius: 20px;
-    padding 0.8rem 0 0.8rem 1.2rem;
-    color: ${theme.colors.formLabel};
-    font-size: 15px;
-    line-height: 18px;
-    position: relative;
-    width: 100%;
-
-    &::placeholder { 
-      color: ${theme.colors.formLabel};
-    }
+    padding-left: 1.2rem;
 
     &:focus-within {
       box-shadow: 0 0 0.5rem ${theme.colors.primary};
@@ -55,19 +18,41 @@ export const Input = styled.input<IconProps>`
   `}
 `
 
-const wrapperModifiers = {
-  icon: () => css`
-    ${Input} {
-      padding-left: 3.5rem;
-    }
-  `
-}
+export const Icon = styled.div`
+  margin-top: 10px;
+`
 
-export const Wrapper = styled.div<IconProps>`
-  ${({ icon }) => css`
-    position: absolute;
-    max-width: 30rem;
+export const Eye = styled.div`
+  margin-top: 8px;
+  margin-right: 10px;
+`
+
+export const Error = styled.div``
+
+export const Input = styled.input<IconProps>`
+  ${({ theme }) => css`
+    color: ${theme.colors.formLabel};
+    font-size: 15px;
+    line-height: 18px;
     width: 100%;
-    ${!icon && wrapperModifiers.icon};
+    height: 1.7rem;
+    background: transparent;
+    border: 0;
+    outline: none;
+
+    &::placeholder {
+      color: ${theme.colors.formLabel};
+    }
+
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none;
+      &::first-line {
+        font-size: ${theme.font.sizes.medium};
+      }
+    }
   `}
 `
+
+export const Wrapper = styled.div``
