c3392cdf6c9c7a3c7565f8ab951bcaad11e54909
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-01 10:54:28 -0300

    feat: add Sign Page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import FormSignin from 'components/FormSignin'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 import Auth from 'templates/Auth'
 
 export default function Index() {
-  return <Auth image="/img/player-1.svg">Form</Auth>
+  const router = useRouter()
+  const path = router.asPath
+  console.log(path)
+
+  useEffect(() => {
+    if (path == '/signin') router.push('/?modal=signin')
+  })
+  return (
+    <Auth title="Fazer o login" image="/img/player-1.svg">
+      <FormSignin />
+    </Auth>
+  )
 }
