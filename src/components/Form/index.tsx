448c8d7e33d61da9cf388a464020978425b38fb5
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-01 10:51:54 -0300

    refactor: margins and colors at Form Component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,17 +1,17 @@
 import styled, { css } from 'styled-components'
 import { darken } from 'polished'
 
-import * as TextFieldStyles from 'components/TextField/styles'
+import * as TextFieldStyles from 'components/Input/styles'
 import * as ButtonStyles from 'components/Button/styles'
 
 export const FormWrapper = styled.div`
   ${({ theme }) => css`
     ${TextFieldStyles.Wrapper} {
-      margin: ${theme.spacings.xxsmall} 0;
+      margin: ${theme.spacings.xsmall} 0;
     }
 
     ${ButtonStyles.Wrapper} {
-      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+      margin: ${theme.spacings.xsmall} auto ${theme.spacings.xsmall};
     }
   `}
 `
@@ -38,19 +38,20 @@ export const FormError = styled.div`
 export const FormLink = styled.div`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.small};
-    color: ${theme.colors.black};
+    color: ${theme.colors.white};
+    font-weight: ${theme.font.semi};
     text-align: center;
+    margin-top: 2rem;
 
     a {
       color: ${theme.colors.secondary};
       text-decoration: none;
-      border-bottom: 0.1rem solid ${theme.colors.bgColor};
-      color: ${theme.colors.bgColor};
+
+      color: ${theme.colors.secondary};
       transition: color, border, ${theme.transition.fast};
 
       &:hover {
-        color: ${darken(0.1, theme.colors.bgColor)};
-        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.bgColor)};
+        color: ${darken(0.1, theme.colors.secondary)};
       }
     }
   `}
