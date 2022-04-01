f02200ba76a0f6c8ba8cdc179514f7883b13dc6c
Fabio Nascimento <agsuperpoder@gmail.com>
2022-04-01 09:50:17 -0300

    test: change method active modal

--- src/components/NavigationButton/test.tsx
+++ src/components/NavigationButton/test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import NavigationButton from '.'
@@ -10,22 +10,5 @@ describe('<NavigationButton />', () => {
     expect(
       screen.getByRole('img', { name: 'open sign in' })
     ).toBeInTheDocument()
-  })
-
-  it('should handle the open/close modal', () => {
-    renderWithTheme(<NavigationButton />)
-
-    const fullElement = screen.getByRole('navigation', { hidden: true })
-
-    expect(fullElement.getAttribute('aria-hidden')).toBe('false')
-    expect(fullElement).toHaveStyle({ opacity: 0 })
-
-    fireEvent.click(screen.getByTestId('openModal'))
-    expect(fullElement.getAttribute('aria-hidden')).toBe('true')
-    expect(fullElement).toHaveStyle({ opacity: 1 })
-
-    fireEvent.click(screen.getByTestId('closeModal'))
-    expect(fullElement.getAttribute('aria-hidden')).toBe('false')
-    expect(fullElement).toHaveStyle({ opacity: 0 })
   })
 })
