0402e37a3003c734b94649230765c5486f43f6f6
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-31 22:07:10 -0300

    feat: add Auth Template mobile

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,10 +1,31 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   display: flex;
   padding: 20px;
+
+  ${media.lessThan('medium')`
+    flex-direction: column;
+  `}
 `
 
-export const Cover = styled.div``
+export const Cover = styled.div`
+  ${media.lessThan('medium')`
+    order: 2;
+    width: 80%;
+    margin: 0 auto;
+  `}
+`
 
-export const FormWrapper = styled.div``
+export const FormWrapper = styled.div`
+  width: 50rem;
+  padding-left: 15rem;
+
+  ${media.lessThan('medium')`
+    order: 1;
+    width: 80%;
+    margin: 0 auto;
+    padding-left: 0;
+  `}
+`
